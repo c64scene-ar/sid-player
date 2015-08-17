e374752fc22a2c9b5f92d69c5d395197918d4ffe
Damián Silvani <munshkr@gmail.com>
2015-08-17 01:14:40 -0300

    Use ca65 constants for C64 registers

--- main.s
+++ main.s
@@ -1,5 +1,7 @@
 .import __MAIN_CODE_LOAD__
 
+.include "c64.inc"  ; C64 constants
+
 ; Convert a hex digit ($00-$0F) to ASCII ('0'-'9' or 'A'-'F')
 .macro hex2asc
     .local skip
@@ -72,28 +74,28 @@ SID_regs_base:
     jsr init_screen   ; clear the screen
     jsr init_text     ; write lines of text
 
-    ldy #$7f          ; $7f = %01111111
-    sty $dc0d         ; turn off CIAs Timer interrupts ($7f = %01111111)
-    sty $dd0d
-    lda $dc0d         ; by reading $dc0d and $dd0d we cancel all CIA-IRQs
-    lda $dd0d         ; in queue/unprocessed.
+    lda #%01111111
+    sta CIA1_ICR      ; turn off CIAs Timer interrupts
+    sta CIA2_ICR
+    lda CIA1_ICR      ; by reading $dc0d and $dd0d we cancel all CIA-IRQs
+    lda CIA2_ICR      ; in queue/unprocessed.
 
     lda #$01          ; set Interrupt Request Mask
-    sta $d01a         ; we want IRQ by Rasterbeam (%00000001)
+    sta VIC_IMR       ; we want IRQ by Rasterbeam (%00000001)
 
     lda #<irq         ; point IRQ Vector to our custom irq routine
     ldx #>irq
-    sta $0314         ; store in $314/$315
-    stx $0315
+    sta IRQVec        ; store in $314/$315
+    stx IRQVec+1
 
     lda #$00          ; trigger interrupt at row zero
-    sta $d012
+    sta VIC_HLINE
 
     lda #%00000011    ; VIC bank #0 ($0000-$3fff)
-    sta $dd00
+    sta CIA2_PRA
 
     lda #%00010110    ; Screen RAM #0001 ($0400-$07FF), Char ROM #011 ($1800-$1FFF)
-    sta $d018
+    sta VIC_VIDEO_ADR
 
     lda #$00
     jsr music_init
@@ -103,7 +105,7 @@ SID_regs_base:
 
 
 irq:
-    dec $d019       ; acknowledge IRQ / clear register for next interrupt
+    dec VIC_IRR     ; acknowledge IRQ / clear register for next interrupt
     jsr music_play
     jsr read_sid
     jmp $ea31       ; return to Kernel routine
@@ -111,17 +113,18 @@ irq:
 
 init_screen:
     ldx #$00
-    stx $d021     ; set background color
-    stx $d020     ; set border color
-
+    stx VIC_BG_COLOR0     ; set background color
+    stx VIC_BORDERCOLOR   ; set border color
 @loop:
     lda #$20      ; #$20 is the spacebar Screen Code
+    ; TODO: Use constant SCREEN_RAM + offset
     sta $0400, x  ; fill four areas with 256 spacebar characters
     sta $0500, x
     sta $0600, x
     sta $06e8, x
 
     lda #$01      ; set foreground to black in Color RAM
+    ; TODO: Use constant SCREEN_RAM + offset
     sta $d800, x
     sta $d900, x
     sta $da00, x
@@ -130,7 +133,6 @@ init_screen:
     inx
     bne @loop
     rts
-
 
 init_text:
     ldx #$00
