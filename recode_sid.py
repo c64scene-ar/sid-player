2d39ca30bca969670af4c66b521d69a5599af923
Damián Silvani <munshkr@gmail.com>
2015-08-17 11:06:30 -0300

    recoder: Use argparse, general refactor

--- recode_sid.py
+++ recode_sid.py
@@ -4,6 +4,7 @@
 from sys import exit, argv
 import re
 import struct
+import argparse
 
 stores = {
     0x8d: "sta %s",
@@ -33,16 +34,16 @@ sidregs_base_addr = 'SID_regs_base'
 
 
 def main():
-    # TODO: Use optparse for command-line options
-    if len(argv) != 4:
-        print("Usage: %s FILE NEW_FILE GEN_ASM_FILE" % argv[0])
-        return 1
+    parser = argparse.ArgumentParser(description='Recode .sid file for use in SID player.')
+    parser.add_argument("sid", help="Original .sid file")
+    parser.add_argument("gen_sid", help="Generated .sid file for player")
+    parser.add_argument("gen_asm", help="Generated .asm file with subroutines for setting shadow variables")
 
-    _, src, dst, gen_asm = argv
+    args = parser.parse_args()
 
     jsr_routines = []
 
-    with open(src, 'rb') as f:
+    with open(args.sid, 'rb') as f:
         data = f.read()
 
     for m in sid_store_re.finditer(data):
@@ -54,18 +55,20 @@ def main():
             idx = len(jsr_routines)
             jsr_routines.append(inst)
 
+        # Calculate subroutine address to jump to
         jsr_offset = idx * jsr_routine_size
         jsr_operand = jsr_base_addr + jsr_offset
-        new_inst = "\x20" + struct.pack("<H", jsr_operand)
 
         print_found_match(m.start(), inst, jsr_operand)
 
+        # Replace Store for JSR in data
+        new_inst = "\x20" + pack_short(jsr_operand)
         data = data[:m.start()] + new_inst + data[m.end():]
 
-    with open(dst, 'wb') as f:
+    with open(args.gen_sid, 'wb') as f:
         f.write(data)
 
-    with open(gen_asm, 'wb') as f:
+    with open(args.gen_asm, 'wb') as f:
         for inst in jsr_routines:
             a = stores[inst['opcode']] % "$%x" % inst['operand']
             b = stores[inst['opcode']] % "%s + $%x" % (sidregs_base_addr, inst['operand'] & 0xff)
@@ -85,6 +88,9 @@ def print_found_match(pos, inst, jsr_operand):
 
 def unpack_short(s):
     return struct.unpack("<H", s)[0]
+
+def pack_short(n):
+    return struct.pack("<H", n)
 
 
 if __name__ == "__main__":
